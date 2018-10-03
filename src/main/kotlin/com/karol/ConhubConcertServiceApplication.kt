6e3124427ada3491a7a1073afec3f27e769310f7
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-10-03 15:21:41 +0200

    mapping to ServerSideEvents class explicitly

--- src/main/kotlin/com/karol/ConhubConcertServiceApplication.kt
+++ src/main/kotlin/com/karol/ConhubConcertServiceApplication.kt
@@ -1,64 +1,17 @@
 package com.karol
 
-import com.karol.domain.Concert
-import com.karol.domain.ConcertComment
-import com.karol.domain.Venue
-import com.karol.handlers.VenueHandler
-import com.karol.repositories.ConcertCommentRepository
-import com.karol.repositories.ConcertRepository
-import com.karol.repositories.VenueRepository
-import org.apache.commons.io.IOUtils
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
-import org.springframework.util.ResourceUtils
-import reactor.core.publisher.Flux
-import java.io.File
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZonedDateTime
-import java.util.*
 
 @SpringBootApplication
 class ConhubConcertServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<ConhubConcertServiceApplication>(*args)
-}
-@Component
-class Bootstrap : CommandLineRunner{
-    @Autowired lateinit var concertRepository: ConcertRepository
-    @Autowired lateinit var venueRepository: VenueRepository
-    @Autowired lateinit var commentRepository: ConcertCommentRepository
-    override fun run(vararg args: String?) {
-        /*venueRepository.deleteAll().block()
-        concertRepository.deleteAll().block()
-
-        
-        val ucho = Venue(id = null, name = "Ucho", avatar = null)
-        val bc = Venue(id = null, name = "Blues Club", avatar = null)
-        val uboga = Venue(id = null, name = "Uboga Krewna", avatar = null)
-        val concert1 = Concert(id = null, venue = ucho, name = "Disco w remizie", date = LocalDateTime.now())
-        val concert2 = Concert(id = null, venue = ucho, name = "1 Rap impra stulecia", date = LocalDateTime.now().plusDays(2))
-        val concert3 = Concert(id = null, venue = ucho, name = "2 Rap impra stulecia", date = LocalDateTime.now().plusDays(12))
-        val concert4 = Concert(id = null, venue = ucho, name = "3 Rap impra stulecia", date = LocalDateTime.now().plusMonths(1))
-        val concert5 = Concert(id = null, venue = bc, name = "4 Rap impra stulecia", date = LocalDateTime.now().plusDays(5))
-        val concert6 = Concert(id = null, name = "Krzysztof Krawczyk", date = LocalDateTime.now().plusDays(7).plusHours(2), venue = uboga)
-        val concert7 = Concert(id = null, name = "Majka Jeżowska", date = LocalDateTime.now().minusHours(10), venue = bc)
-        val concert8 = Concert(id = null, name = "Zdzisława Sośnicka", date = LocalDateTime.now().plusDays(6).minusHours(2), venue = uboga)
-        val concert9 = Concert(id = null, name = "Deadmau5", date = LocalDateTime.now().plusMonths(3).plusDays(8), venue = uboga)
-
-        venueRepository.saveAll(Flux.just(ucho,bc, uboga)).blockLast()
-        concertRepository.saveAll(Flux.just(concert1, concert2, concert3, concert4, concert5, concert6, concert7, concert8, concert9)).blockLast()
-
-        val comment = ConcertComment(id = null, text = "Ten koncert jest zajebisty!", concert = concertRepository.findByName(concert1.name).block()?:concert1, timestamp = LocalDateTime.now(), username = "username")
-        commentRepository.save(comment).block()
-
-*/
-        }
-
+    SpringApplication(ConhubConcertServiceApplication::class.java).apply {
+        run(*args)
     }
+}
+
+
 
 
--- src/main/kotlin/com/karol/handlers/ConcertCommentHandler.kt
+++ src/main/kotlin/com/karol/handlers/ConcertCommentHandler.kt
@@ -7,6 +7,7 @@ import com.karol.services.ConcertService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.codec.ServerSentEvent
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -26,7 +27,7 @@ class ConcertCommentHandler{
     lateinit var webClientService: WebClientService
     val DEFAUTL_PAGE = 0L
     val DEFAULT_SIZE = 10L
-    val sseProcessor = EmitterProcessor.create<ConcertComment>()
+    val sseProcessor = EmitterProcessor.create<ServerSentEvent<ConcertComment>>()
 
     fun commentsByConcertFlux(page: Long?, size: Long?, by: String?, direction: String?, concertId: String): Flux<ConcertComment> =
         concertCommentService.findAllByConcertId(concertId= concertId, by = by, direction = direction)
@@ -57,12 +58,11 @@ class ConcertCommentHandler{
     fun saveCommentMono(comment: Mono<ConcertCommentDto>, concertId: String, username: String): Mono<ConcertComment> {
         return concertService.findById(concertId)
                 .switchIfEmpty(Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND, "Concert not found")))
-                //TODO: jak sie ogarnie to włączyć
                 //.filterWhen { webClientService.checkUsernameAsync(username) }
                 .zipWith(comment)
                 .map { ConcertComment(id = null, text = it.t2.text, timestamp = it.t2.timestamp?: LocalDateTime.now(), concert = it.t1, username = username) }
                 .flatMap(concertCommentService::saveComment)
-                .doOnSuccess { sseProcessor.onNext(it) }
+                .doOnSuccess { sseProcessor.onNext(convertCommentToServerSentEvent(it)) }
                 .switchIfEmpty(Mono.error(ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Save unsuccessful")))
     }
     fun deleteCommentById(req: ServerRequest): Mono<ServerResponse> = ServerResponse.accepted().body(
@@ -70,8 +70,8 @@ class ConcertCommentHandler{
                     .filter { it.username == req.pathVariable("username") }
                     .flatMap { concertCommentService.deleteCommentById(it.id?:"") }
     )
-    fun commentsSseResponse(req: ServerRequest) = ServerResponse.ok().contentType(MediaType.APPLICATION_STREAM_JSON).body(this.sseProcessor.share())
-
+    fun commentsSseResponse(req: ServerRequest) = ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(this.sseProcessor.share())
+    private fun convertCommentToServerSentEvent(comment: ConcertComment): ServerSentEvent<ConcertComment> = ServerSentEvent.builder(comment).comment("comment").build()
 
 }
 
